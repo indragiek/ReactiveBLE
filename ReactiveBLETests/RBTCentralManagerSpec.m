36bc929b3a0e921ba78b7231d8d49f4626c8f510
Indragie Karunaratne <i@indragie.com>
2014-02-26 19:20:37 -0700

    Set up specta and expecta
    Wow, much complicated.

---
+++ ReactiveBLETests/RBTCentralManagerSpec.m
@@ -0,0 +1,11 @@
+//
+//  RBTCentralManagerSpec.m
+//  ReactiveBLE
+//
+//  Created by Indragie Karunaratne on 2014-02-26.
+//  Copyright (c) 2014 Indragie Karunaratne. All rights reserved.
+//
+
+SpecBegin(RBTCentralManager)
+
+SpecEnd
