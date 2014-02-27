c6edc25e14922bc12be7a20ed3c852a79ae85db6
Indragie Karunaratne <i@indragie.com>
2014-02-27 09:19:37 -0700

    Add stubs of all the delegate methods

---
+++ ReactiveBLE/RBTCentralManager+DelegateStubs.h
@@ -0,0 +1,15 @@
+//
+//  RBTCentralManager+DelegateStubs.h
+//  ReactiveBLE
+//
+//  Created by Indragie Karunaratne on 2014-02-27.
+//  Copyright (c) 2014 Indragie Karunaratne. All rights reserved.
+//
+
+#import <ReactiveBLE/ReactiveBLE.h>
+
+/**
+ *  Stub implementations of all the `CBCentralManagerDelegate` methods.
+ */
+@interface RBTCentralManager (DelegateStubs)
+@end
